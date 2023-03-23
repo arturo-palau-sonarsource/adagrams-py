2aa2485c372a9bb0d26cd83eb67a44a12fd249ff
Virginia Ramos <virginia.rare@gmail.com>
2023-03-22 21:25:02 -0500

    Pass First Wave

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,49 @@
+import random
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
-    pass
+    # Create a copy of the letter pool dictionary
+    letter_pool = LETTER_POOL.copy()
+
+    # Draw 10 letters from the pool
+    letters = []
+    for i in range(10):
+        letter = random.choice(list(letter_pool.keys()))
+        while letter_pool[letter] == 0:
+            letter = random.choice(list(letter_pool.keys()))
+        letters.append(letter)
+        letter_pool[letter] -= 1
+
+    # Return the list of drawn letters
+    return letters
 
 def uses_available_letters(word, letter_bank):
     pass
